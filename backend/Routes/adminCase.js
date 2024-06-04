7944a225b45a43cba516dea654d5f77d6f4ac5e7
themohdishaq <ishaq10866@gmail.com>
2024-06-04 22:53:05 +0500

    update with functioning

--- backend/Routes/adminCase.js
+++ backend/Routes/adminCase.js
@@ -10,7 +10,41 @@ const Notifications = require("../Models/Notifications");
 const Student = require("../Models/Student");
 const Donor = require("../Models/Donor");
 const WorksOn = require("../Models/WorksOn");
+const nodemailer = require("nodemailer");
 
+router.post("/mailing", async (req, res) => {
+    const { email, subject, message } = req.body; // Extract email, subject, and message from request body
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: subject, // Set subject from request body
+            html: `<h1>${message}</h1>` // Set message from request body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("Error", error);
+                res.status(500).json({ error: "Failed to send email" });
+            } else {
+                console.log("Email sent:", info.response);
+                res.status(201).json({ status: 201, message: "Email sent successfully" });
+            }
+        });
+    } catch (error) {
+        console.log("Error", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 //route to approve case by admin
 router.post(
   "/admin/case-approve-by-admin/:id",
